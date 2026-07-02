276ad6aaafc064c275a1fb91607e2154d62e6472
Jesse Panganiban <hello@jpanganiban.com>
2026-07-02 16:06:48 +0800

    Initial Commit

---
+++ Summoner/SummonerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SummonerApp.swift
+//  Summoner
+//
+//  Created by Jesse Panganiban on 7/2/26.
+//
+
+import SwiftUI
+
+@main
+struct SummonerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
